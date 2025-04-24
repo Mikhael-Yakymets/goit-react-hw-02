4c3d6ed7f22ded520816a52179b7f4cbcd0f069e
Mykhailo Yakymets <vskitano@gmail.com>
2025-04-24 21:59:24 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,17 @@
-// import css from './App.module.css';
-import userData from '../userData.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
-
-import Profile from './Profile/Profile';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
+import Feedback from './Feedback/Feedback';
+import Options from './Options/Options';
 
 const App = () => {
+  const propsComments = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Feedback />
+      <Options />
     </>
   );
 };
